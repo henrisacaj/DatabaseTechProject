4b22bfe02ca9e14dd0331de487ed9afe67d0bcb1
Omar Schure <omar.schure@informatik.hs-fulda.de>
2022-10-12 20:56:23 +0200

    functions finished

--- Testing/app.js
+++ Testing/app.js
@@ -18,7 +18,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "db-tech",
+    password: "2774",
     database: "gym",
 
 });
@@ -53,32 +53,36 @@ app.post('/add', jsonParser, function(req,res){
 
 
 
-app.post('/add_customer', jsonParser, function(req,res){
-    console.log(req.body.membership_id);
+app.post('/add_customer', function(req,res){
+    //console.log(req.body.membership_id);
     //res.setHeader('Content-Type', 'text/plain')
     //res.write('you posted:\n')
     //res.end(JSON.stringify(req.body, null, 2))
-    console.log("Using Body-parser: ", req.body)
+    //console.log("Using Body-parser: ", req.body)
     
     
     var  create_new_customer = "CALL create_new_customer(?, ?, ?, ?, ?, ?, ?, ?, ?);"
     con.query(create_new_customer, [req.body.membership_id, req.body.Firstname, req.body.Lastname, req.body.street, req.body.zipcode, req.body.email, req.body.phone, req.body.iban, req.body.birthday], function (err, result) {
         if (err){
-            var message = "Fehler beim einfügen!!";
+            var message = { mess: "Der Kunde konnte nicht hinzugefügt werden!",
+                firstname: "",
+                lastname: ""
+            };
             res.render(
-                'successCust.pug',
-                {message: message, firstname: req.body.Firstname , lastname: req.body.Lastname}
+                'customer.pug',
+                {custMesAd: message}
             )
 
-            //throw err;
+          
 
         } 
         else {console.log("Customer created:");
         console.log(result);
-        var message = { Mess: "Der Kunde wurde erfolgreich hinzugefügt",
+        var message = { mess: "Der Kunde wurde erfolgreich hinzugefügt: ",
                 firstname: req.body.Firstname,
                 lastname: req.body.Lastname
         };
+       
         res.render(
             'customer.pug',
             {custMesAd: message}
@@ -97,9 +101,7 @@ app.listen(8080, function(){
     console.log("server is running on port 8080");
 })
 
-app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname,'./index.html'));
-});
+
 
 
 
@@ -108,28 +110,97 @@ app.get('/', function(req,res){
 //PUG yes yes
 var  get_view_extras = "SELECT * FROM gym.membership_extra;"
 
-app.get('/mainsite', function(req, res){
+app.get('/', function(req, res){
 
     con.query(get_view_extras, function(error, results, fields) {
         if(error);
         else {
             console.log('The solution is: ', results);
             res.render(
-                'test.pug',
+                'index.pug',
                 {dataMemb: results}
             )
         }
     })
-
-
-
-
 })
+
+
 
 app.get('/customer', function(req, res){
-    
+    var message = { Mess: "",
+    firstname: "",
+    lastname: ""
+    };
     res.render(
         'customer.pug',
-        {}
+        {custMesAd: message}
     )
 })
+
+
+app.post('/deleteCustomer', function(req,res){
+    var message = { mess: "",
+        firstname: "",
+        lastname: "",
+        deleteMessage: "" 
+    };
+    var  delete_customer = "CALL delete_customer(" + req.body.customerNumber + ");"
+    con.query(delete_customer, function (error, results, field) {
+        if (error){
+            message.mess ="";
+            message.firstname = "";
+            message.lastname = "";
+            message.deleteMessage = "Der Kunde konnte nicht gelöscht werden!";
+        } 
+        else {
+
+        console.log(results);
+        message.mess = "";
+        message.firstname = "";
+        message.lastname =  "";
+        message.deleteMessage = "Der Kunde wurde erfolgreich gelöscht!";
+
+        }
+        res.render(
+            'customer.pug',
+            {custMesAd: message}
+        )
+        
+    });
+
+   
+
+});
+
+
+app.post('/findCustomer', function(req,res){
+    var message = { mess: "",
+        firstname: "",
+        lastname: "",
+        deleteMessage: "",
+        findMessage: ""
+    };
+    var  find_customer = "SELECT get_customer_id_by_name (?,?,?) as id;"
+    con.query(find_customer, [req.body.Firstname, req.body.Lastname, req.body.birthday],  function (error, results, field) {
+        
+ 
+
+        if (error){
+            message.findMessage = "Diesen Kunden gibt es nicht!";
+        } 
+        else {
+       
+            console.log("Die ID ist: " + results[0].id);
+            message.findMessage = "Die ID von " + req.body.Firstname + " " + req.body.Lastname + " ist: " + results[0].id;
+
+        }
+        res.render(
+            'customer.pug',
+            {custMesAd: message}
+        )
+        
+    });
+
+   
+
+});
